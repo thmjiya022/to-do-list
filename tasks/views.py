c8fbc97e59a11d007ccfb803fd4219c671e84dc4
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-21 22:42:35 +0200

    updated models and views

--- tasks/views.py
+++ tasks/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+from .models import *
+
 def index(request):
-    return HttpResponse('Hello World')
+    tasks = Task.objects.all()
+
+    context = {'tasks': tasks}
+    return render(request, 'tasks/lists.html', context)
