c35201f41564ad22f473286e22f0dc8d9c97caa0
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-22 13:43:33 +0200

    updated index views

--- tasks/views.py
+++ tasks/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 
 from .models import *
+from .forms import *
 
 def index(request):
     tasks = Task.objects.all()
 
-    context = {'tasks': tasks}
+    form = TaskForm()
+
+    if  request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context = {'tasks': tasks, 'form': form}
     return render(request, 'tasks/lists.html', context)
+
