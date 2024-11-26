5475e9684bf1a9ae96bd4393bcb55d166d1ad566
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-26 09:44:33 +0200

    add due_date field

--- tasks/models.py
+++ tasks/models.py
@@ -6,6 +6,7 @@ class Task(models.Model):
     title = models.CharField(max_length=200)
     complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.title
