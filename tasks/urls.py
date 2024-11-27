a8cf490e2572dcddd41cd30d37984e5469d427d2
thmjiya022 <mjiyakhothobani@gmail.com>
2024-11-27 22:55:44 +0200

    added login url

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='list'),
+    path('login/', views.user_login, name='login'),
     path('update_task/<str:pk>/', views.updateTask, name='update_task'),
     path('delete/<str:pk>/' ,views.deleteTask, name='delete'),
 ]
